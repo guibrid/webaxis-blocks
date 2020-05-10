f347cc50641881bcadfcc129dca53c6085b65873
Guillaume <guillaume.briard@gmail.com>
2020-05-10 22:12:11 +0200

    initial commit

---
+++ webaxis-blocks.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* Plugin Name: Webaxis Blocks
+* Plugin URI: https://www.web-axis.biz/
+* Description: Usefull blocks for Wordpress Gutenberg
+* Version: 1.0
+* Author: Guillaume Briard
+**/
